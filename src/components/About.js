a351a46c57dbd48646bb12c62d2447529a97faa0
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-06-09 21:12:32 +0100

    finished about section

--- src/components/About.js
+++ src/components/About.js
@@ -9,39 +9,39 @@ const About = () => {
             img: people,
             h1: 'Insight into your distribution chain',
             p: 'Keana connects all possible software systems, resulting in a single, simple overview of the new decentralised distribution chain.',
-            flex: 'right'
+            flex: 'left'
         },
         {
             id: 2,
             img: laptop,
             h1: 'Improve efficiency, lower costs.',
             p: 'Keana connects all possible software systems, resulting in a single, simple overview of the new decentralised distribution chain.',
-            flex: 'left'
+            flex: 'right'
         },
         {
             id: 3,
             img: bicycle,
             h1: 'Be accountable for your Co2 footprint',
             p: "Find and select smart mobility vehicles, reducing your overall carbon emissions. And get instant insight into the CO2 reduction you've achieved.",
-            flex: 'right'
+            flex: 'left'
         },
 
 
     ]
     return (
-        <div className='bg-white pt-[4rem] w-[100vw] px-[2rem]'>
-            <h1 className=' text-slate-500 font-mono text-3xl border font-bold'>
+        <div className='bg-white py-[4rem] w-[100vw] px-[2rem] lg:px-[25%] md:px-[10%]'>
+            <h1 className=' text-slate-500 lg:text-center font-mono text-3xl font-bold'>
                 Keana is the solution: manage your shipments through all handlers.
             </h1>
-            <div className='mt-[5rem] flex flex-col'>
+            <div className='mt-[5rem] lg:mt-[3rem] flex flex-col'>
                 {
                     object.map((item) => {
                         return (
-                            <div key={item.id} className={`flex mt-[6rem] flex-col justify-center items-center ${item.flex === 'right' ? 'lg:flex-row' : 'flex-row-reverse'}`}>
+                            <div key={item.id} className={`flex md:justify-between mt-[6rem] flex-col justify-center items-center ${item.flex === 'right' ? 'lg:flex-row md:flex-row' : 'lg:flex-row-reverse md:flex-row-reverse'}`}>
                                 <div className='w-[18rem] h-[18rem] rounded-full overflow-hidden'>
                                     <img src={item.img} alt='image' className='w-[100%] h-3s120%] rounded-full' />
                                 </div>
-                                <div className='mt-[4rem]'>
+                                <div className='mt-[4rem] md:w-[40%] lg:w-[50%]'>
                                     <h1 className='text-purple-400 text-2xl font-mono  font-bold'>{item.h1}</h1>
                                     <p className='mt-[1.2rem] font-mono text-[15px]'>{item.p}</p>
                                     <button className='mt-[2rem] border px-5 font-semibold py-3 text-purple-400 font-mono  rounded-3xl hover:bg-purple-400 hover:text-white'>Learn more &gt;</button>

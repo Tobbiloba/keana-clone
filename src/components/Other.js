e70f7ddcf967a8662412594a96aa90dbe3e65a58
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-06-09 17:29:30 +0100

    testing

--- src/components/Other.js
+++ src/components/Other.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Other = () => {
     return (
-        <div className='w-[100vw] bg-cyan-100 lg:items-center z-30 lg:flex flex-row lg:h-[650px] md:flex md:justify-end md:h-[35rem]'>
+        <div className='w-[100vw] bg-slate-500 lg:items-center z-30 lg:flex flex-row lg:h-[650px] md:flex md:justify-end md:h-[35rem]'>
             {/* <div className='hidden lg:flex  w-[45%] overflow-hidden'>
 
             </div> */}
             {/* <div className='absolute left-0 overflow-hidden w-[60vw] md:w-[40vw] hidden lg:flex md:flex md:h-[35rem]'>
                 <img src="https://keana.io/assets/vector-nodes-green.937a46d6.svg" alt="image" className='max-w-none relative md:w-[75rem]' />
             </div> */}
-            <div className=' px-[2rem] z-10 bg-cyan-100 pt-[2.5rem] md:w-[70%] md:h-[35rem] lg:w-[45%] h-fit '>
-                <h1 className='text-green-400 text-4xl font-bold font-mono '>More handlers,<br />more problem</h1>
-                <p className='mt-6 text-[18px] leading-8 text-green-800 '>
+            <div className=' px-[2rem] z-10 md:bg-cyan-100 pt-[2.5rem] md:w-[70%] md:h-[35rem] lg:w-[45%] h-fit '>
+                <h1 className='text-slate-700 text-4xl font-bold font-mono '>More handlers,<br />more problem</h1>
+                <p className='mt-6 text-[18px] leading-8 text-slate-300 '>
                     The logistics industry is rapidly advancing towards a future with fewer heavy trucks on the streets, resulting in reduced pollution and safer cities. Central to this progress is the emergence of distribution hubs. While distribution hubs offer a brilliant solution, they also introduce the challenge of multiple handlers utilizing various logistics software within the decentralized distribution chain, potentially causing congestion in the existing system.
                 </p>
             </div>
-            <div className='flex lg:hidden mt-4 md:hidden border w-[100vw]'>
+            <div className='flex lg:hidden mt-4 md:hidden w-[100vw]'>
 
                 <img src="https://keana.io/assets/vector-nodes-green-mobile.8661594c.svg" alt="image" className='flex  lg:hidden w-[100vw]' />
             </div>

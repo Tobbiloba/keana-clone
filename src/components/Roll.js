1eef17948fd2c3dcea64217ef7290d3b46b4f443
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-06-10 17:20:21 +0100

    removed center item

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Roll = () => {
     return (
-        <div className='py-[5rem] px-[1.5rem] flex flex-col items-center md:justify-between lg:justify-center lg:gap-[15%] md:px-[3rem] md:flex-row'>
+        <div className='py-[5rem] px-[1.5rem] flex flex-col md:items-center lg:items-center md:justify-between lg:justify-center lg:gap-[15%] md:px-[3rem] md:flex-row'>
             <h1 className='text-slate-700 text-[80px] leading-[1.3] font-bold font-mono'>Let it <br />roll</h1>
             <div className='mt-[3rem] md:w-[50%] lg:w-[30%]'>
                 <p className='text-slate-500 text-[15px] md:leading-7 font-mono'>At Keana we believe that fuzzy algorithms, embracing a certain amount of uncertainty and freedom in human behaviour, always lead to the best logistics solutions. Keana does the maths, leaving creativity, intuition and experience to the people using our platform.

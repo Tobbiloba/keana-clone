b63b34e679a8ec5601ffd4a8f8ff7393abb94af1
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-06-13 22:49:18 +0100

    finished manifesto page mobile responsiveness

--- src/screen/AboutPage.js
+++ src/screen/AboutPage.js
@@ -2,11 +2,17 @@ import React from 'react'
 // import img from '../assets/Keana_files/'
 import Menu from '../components/Menu'
 import AboutRoll from '../components/AboutRoll'
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Manifesto from '../components/Manifesto';
 const AboutusPage = () => {
     return (
         <div>
             <Menu />
             <AboutRoll />
+            <Manifesto />
+            <Banner />
+            <Footer />
         </div>
     )
 }
